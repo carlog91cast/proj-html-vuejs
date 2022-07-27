d91f8cab7b9804226823350954bce8e29a02c400
bigeppo <carlog91cast@gmail.com>
2022-07-27 11:03:13 +0200

    add img and text in components

--- src/components/KnowHow.vue
+++ src/components/KnowHow.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col-6 img-section">
-            <img src="../assets/img/stock-full-hd-11-670x450.jpg" alt="img">
-			<img  class="range" src="../assets/img/icon-youtube-play.png" alt="img2">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 img-section">
+                <img src="../assets/img/stock-full-hd-11-670x450.jpg" alt="img">
+                <img class="range" src="../assets/img/icon-youtube-play.png" alt="img2">
+            </div>
+            <div class="col-6 text-section">
+                <h6>HOW WE WORK</h6>
+                <H1>Upgrade Your <br> Skills
+                    <span>Upgrade <br> Your Life</span>
+                </H1>
+                <p>Opening up more opportunites in front of youy to get the <br> ticket to more enjoyable door in life.
+                    Getting equipped
+                    with <br> new skills for new set of insights into yoit career. <br> MaxCoach accompany learnes in every stage
+                    of learning
+                </p>
+                <div><a href=""><strong>Download free guidebook</strong></a></div>
+            </div>
         </div>
-        <div class="col-6 text-section"></div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -19,4 +30,5 @@ export default {
 <style lang="scss" scoped>
 @import "~bootstrap/scss/bootstrap";
 @import "../assets/style/common.scss";
+
 </style>
