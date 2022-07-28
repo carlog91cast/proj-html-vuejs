64d78d1caefaafdf6fb94f14a83989473b8dbaa5
bigeppo <carlog91cast@gmail.com>
2022-07-28 15:27:35 +0200

    add footer array

--- src/components/ProjFooter.vue
+++ src/components/ProjFooter.vue
@@ -1,7 +1,16 @@
 <template>
-    <div>
-        
-    </div>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                    
+                </div>
+                <div class="col-10"></div>
+            </div>
+
+        </div>
+    </footer>
+
 </template>
 
 <script>
@@ -9,7 +18,68 @@
 
 export default {
     name: 'ProjFooter',
+    data() {
+        return {
+            menuList: [
+                {
+                    text: 'Start here',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Blog',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'About us',
+                    url: '#',
+                    current: true
+                },
 
+            ],
+            menuListOne: [
+                {
+                    text: 'Success story',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Coursess',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Contact us',
+                    url: '#',
+                    current: true
+                },
+            ],
+            menuListaTwo: [
+                {
+                    text: 'Membership',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Punchase guide',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Privacy Policy',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'Team of service',
+                    url: '#',
+                    current: true
+                },
+            ]
+        }
+
+    }
 }
 </script>
 
