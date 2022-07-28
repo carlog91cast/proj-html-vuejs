9a10e2a6b4ec1ba3ac4db7e19b3f5335246a2916
bigeppo <carlog91cast@gmail.com>
2022-07-28 12:18:10 +0200

    ourBlogs end

--- src/components/OurBlogs.vue
+++ src/components/OurBlogs.vue
@@ -21,9 +21,9 @@
                         <div class="description">
                             {{ artist.description }}
                         </div>
-                        <div class="students mt-4">
+                        <div class="students mt-5">
                             <span>{{ artist.date }}</span>
-                            <span>{{ artist.views }}</span>
+                            <span class="mx-5">{{ artist.views }}</span>
                         </div>
                     </div>
                 </div>
@@ -40,22 +40,22 @@ export default {
             artistList: [
                 {
                     img: 'artist-blog-03-480x325.jpeg',
-                    title: 'artist',
-                    description: 'Brush strokes energize trees in paintings',
+                    title: 'ARTIST',
+                    description: 'Brush strokes energize trees in colorful paintings',
                     date: 'May 15, 2020',
                     views: '688 views',
                 },
                 {
                     img: 'artist-blog-01-480x325.jpg',
-                    title: 'artist',
+                    title: 'ARTIST',
                     description: 'Pocket-Sized notebooks hold miniature paintings',
                     date: 'May 15, 2020',
-                    views: '603 Views<',
+                    views: '603 Views',
                    
                 },
                 {
                     img: 'artist-blog-02-480x325.jpg',
-                    title: 'artist',
+                    title: 'ARTIST',
                     description: 'connection Between Self-Portaits and identity',
                     date: 'May 15, 2020',
                     views: '397 Views',
@@ -96,7 +96,10 @@ export default {
     background-color: white;
     padding: 0;
     width: 30%;
-
+    .text{
+        margin-left: 8px;
+    }
+   
 }
 
 .cart:hover {
@@ -104,11 +107,10 @@ export default {
 }
 
 .price {
-    font-size: 25px;
-    font-weight: bold;
-    color: #1fad95;
+    font-size: 15px;
+    font-weight: 300;
+    color:#6c6c6b;
     margin-top: 15px;
-    margin-left: 20px;
 }
 
 .price:hover {
@@ -118,14 +120,11 @@ export default {
 .description {
     font-size: 15px;
     font-weight: 600;
-    margin: 20px;
+    margin-top: 15px;
 }
 
 .students {
     color: #6c6c6b;
 
-    span {
-        margin-left: 20px;
-    }
 }
 </style>
